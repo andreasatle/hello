6dc88078d052da93148e6c63e0c55341354b95ea
Andreas Atle <atle.andreas@gmail.com>
2022-09-13 11:45:06 -0500

    Update

--- curl.sh
+++ curl.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 URL=localhost:3000
-myfunction () {
+getHTML () {
     ARG=$URL/name/$1
     echo Calling HTTP-server: curl $ARG
     curl $ARG
 }
 
-myfunction 01-Andy &
-myfunction 02-Brett &
-myfunction 03-Cesar &
-myfunction 04-David &
-myfunction 05-Eric &
-myfunction 06-Felix &
-myfunction 07-Greg &
-myfunction 08-Hardy &
-myfunction 09-Indy &
-myfunction 10-JarJar &
+getHTML 01-Andy &
+getHTML 02-Brett &
+getHTML 03-Cesar &
+getHTML 04-David &
+getHTML 05-Eric &
+getHTML 06-Felix &
+getHTML 07-Greg &
+getHTML 08-Hardy &
+getHTML 09-Indy &
+getHTML 10-JarJar &
